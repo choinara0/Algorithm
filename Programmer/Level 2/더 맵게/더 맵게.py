7134c9305ce31537f9e5e7975114b7982cea17fe
choinara0 <chlskfk6810@naver.com>
2021-05-30 16:58:07 +0900

    push solution level2 - 문자열 압축

--- Programmer/Level 2/더 맵게/더 맵게.py
+++ Programmer/Level 2/더 맵게/더 맵게.py
@@ -56,21 +56,20 @@ import heapq
 #힙의 특성은 가장 작은 요소가 항상 루트인 heap[0]이다.
 def solution(scoville, K):
     answer = 0
-    heaplist = list()
-    for i in scoville:
-        heapq.heappush(heaplist, i)
-    while(len(heaplist)>1):
-        if heaplist[0] < K:
-            a = heapq.heappop(heaplist)
-            b = heapq.heappop(heaplist)
+    heapq.heapify(scoville)
+    while(len(scoville)>1):
+        if scoville[0] < K:
+            a = heapq.heappop(scoville)
+            b = heapq.heappop(scoville)
             temp = a + (b*2)
-            heapq.heappush(heaplist, temp)
+            heapq.heappush(scoville, temp)
             answer += 1
         else:
             break
-    for i in heaplist:
+    for i in scoville:
         if i<K:
             return -1
+
     return answer
 
 solution([1, 2, 3, 9, 10, 12], 7)
