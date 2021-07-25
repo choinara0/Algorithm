b9ca3a800eff6864faf50372ef2753c3e8e27dea
choinara0 <chlskfk6810@naver.com>
2021-07-25 16:25:10 +0900

    push solution 가장 큰 정사각형

--- Programmer/Level 2/가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
+++ Programmer/Level 2/가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.py
@@ -1,11 +1,37 @@
 def solution(board):
-    answer = 1234
-    for i in range(len(board)):
-        check = 0
-        for j in range(len(board[i])):
-            if j==1:
-                print(a)
+    answer = 0
+    n = len(board) #세로
+    m = len(board[0]) #가로
+
+    dp = [[0]*m for i in range(n)]
+    dp[0] = board[0]
+    for i in range(1, n):
+        dp[i][0] = board[i][0]
+
+    for i in range(1, n):
+        for j in range(1, m):
+            if board[i][j] == 1:
+                dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
+
+    for i in range(n):
+        temp = max(dp[i])
+        answer = max(answer ,temp)
+    answer = answer ** 2
 
     return answer
 
 solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]])
+
+'''
+board 
+0 1 1 1
+1 1 1 1
+1 1 1 1
+0 0 1 0
+
+dp
+0 1 1 1 
+1 1 2 2 
+1 2 2 3 
+0 0 2 0
+'''
