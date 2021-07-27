4d9abdc5dffe41c72f0f90feb4f9e69a219a203b
choinara0 <chlskfk6810@naver.com>
2021-07-28 00:07:18 +0900

    push solution 1,2,3 더하기

--- Baekjoon/Dynamic Programming/1, 2, 3 더하기/1, 2, 3 더하기.py
+++ Baekjoon/Dynamic Programming/1, 2, 3 더하기/1, 2, 3 더하기.py
@@ -2,13 +2,21 @@ import sys
 T = int(sys.stdin.readline())
 for i in range(T):
     n = int(sys.stdin.readline())
-
-    dp = [[0] for i in range(n)]
-    dp[0] = 1
-    dp[1] = 2
-    dp[2] = 4
-    for i in range(3, len(dp)):
-        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
-
-    print(dp)
+    if n == 1:
+        print(1)
+        continue
+    elif n == 2:
+        print(2)
+        continue
+    elif n == 3:
+        print(4)
+        continue
+    elif n>3:
+        dp = [[0] for i in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        dp[2] = 4
+        for i in range(3, len(dp)):
+            dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+    print(dp[-1])
 
