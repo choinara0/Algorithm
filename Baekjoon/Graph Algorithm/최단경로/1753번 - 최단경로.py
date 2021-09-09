46b2dd925b41b69f1ce0aaac01b6697d84c1dfed
choinara0 <chlskfk6810@naver.com>
2021-09-09 23:38:08 +0900

    push 최단경로.py solution

--- Baekjoon/Graph Algorithm/최단경로/1753번 - 최단경로.py
+++ Baekjoon/Graph Algorithm/최단경로/1753번 - 최단경로.py
@@ -1,1 +1,32 @@
+import sys
+import collections
+import heapq
 
+V, E = map(int, sys.stdin.readline().split())
+start_v = int(sys.stdin.readline())
+graph = collections.defaultdict(list)
+
+for i in range(E):
+    u, v, w = map(int, sys.stdin.readline().split())
+    graph[u].append((v, w))
+
+def daikstra(graph, V, start_v):
+    q = [(0, start_v)]
+    distance = collections.defaultdict(int)
+
+    while q:
+        dist, node = heapq.heappop(q)
+        if node not in distance:
+            distance[node] = dist
+            for v, w in graph[node]:
+                update = dist + w
+                heapq.heappush(q, (update, v))
+    for i in range(1, V+1):
+        if i==start_v:
+            print('0')
+        elif not distance[i]:
+            print('INF')
+        else:
+            print(distance[i])
+
+daikstra(graph, V, start_v)
