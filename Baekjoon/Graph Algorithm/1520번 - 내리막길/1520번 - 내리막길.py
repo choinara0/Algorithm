2186e599e22349e1ba61037af0171b4da52c62b9
choinara0 <chlskfk6810@naver.com>
2021-09-15 14:37:12 +0900

    push 내리막길.py solution

---
+++ Baekjoon/Graph Algorithm/1520번 - 내리막길/1520번 - 내리막길.py
@@ -0,0 +1,23 @@
+import sys
+
+M, N = map(int, sys.stdin.readline().split())
+matrix = []
+for i in range(M):
+    matrix.append(list(map(int, sys.stdin.readline().split())))
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+def dfs(x, y):
+    if x == 0 and y == 0:
+        return 1
+    if dp[x][y] == -1:
+        dp[x][y] = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (0 <= nx < M and 0 <= ny < N ) and matrix[x][y] < matrix[nx][ny]:
+                dp[x][y] += dfs(nx, ny)
+
+    return dp[x][y]
+
+dp = [[-1]*N for i in range(M)]
+print(dfs(M-1, N-1))
