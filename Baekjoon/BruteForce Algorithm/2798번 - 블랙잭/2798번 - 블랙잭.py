07ecf2e4a2468cd286d75d0181209958fb360ebb
choinara0 <chlskfk6810@naver.com>
2021-09-19 12:35:09 +0900

    push 블랙잭.py solution

---
+++ Baekjoon/BruteForce Algorithm/2798번 - 블랙잭/2798번 - 블랙잭.py
@@ -0,0 +1,12 @@
+import sys
+from itertools import combinations
+N, M = map(int, sys.stdin.readline().split())
+num = list(map(int, sys.stdin.readline().split()))
+a = combinations(num, 3)
+answer = 0
+for i in a:
+    if sum(i)<=M and sum(i)>answer:
+        answer = sum(i)
+print(answer)
+
+
