9175cc360151a46721e5f269c33fbe5d7b139286
choinara0 <chlskfk6810@naver.com>
2021-09-19 12:38:24 +0900

    코드 수정

--- Baekjoon/BruteForce Algorithm/2798번 - 블랙잭/2798번 - 블랙잭.py
+++ Baekjoon/BruteForce Algorithm/2798번 - 블랙잭/2798번 - 블랙잭.py
@@ -5,7 +5,10 @@ num = list(map(int, sys.stdin.readline().split()))
 a = combinations(num, 3)
 answer = 0
 for i in a:
-    if sum(i)<=M and sum(i)>answer:
+    if sum(i) == M:
+        answer = sum(i)
+        break
+    elif sum(i)<M and sum(i)>answer:
         answer = sum(i)
 print(answer)
 
