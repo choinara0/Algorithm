e8540e72f3cb677b42fcd740abe476f31ff36a21
choinara0 <chlskfk6810@naver.com>
2021-09-29 15:02:50 +0900

    push N과 M(6).py solution

---
+++ Baekjoon/BackTracking/15655번 - N과 M(6)/15655번 - N과 M(6).py
@@ -0,0 +1,21 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+num = sorted(list(map(int, sys.stdin.readline().split())))
+result = []
+def dfs():
+    if len(result) == M:
+        print(' '.join(map(str, result)))
+        return
+    else:
+        for i in range(len(num)):
+            if len(result) == 0:
+                result.append(num[i])
+                dfs()
+                result.pop()
+            elif result[-1] < num[i]:
+                result.append(num[i])
+                dfs()
+                result.pop()
+    return
+
+dfs()
