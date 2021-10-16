464daf59bc42349e221c2bde166a524253f6696f
choinara0 <chlskfk6810@naver.com>
2021-10-16 16:25:11 +0900

    push 절대값 힙.py solution

---
+++ Baekjoon/PriorityQueue/11286번 - 절대값 힙/11286번 - 절대값 힙.py
@@ -0,0 +1,17 @@
+import sys
+from queue import PriorityQueue
+
+N = int(sys.stdin.readline())
+priorityQueue = PriorityQueue(maxsize=N)
+
+for i in range(N):
+    temp = int(sys.stdin.readline())
+
+    if temp == 0 and priorityQueue.qsize() == 0:
+        print(0)
+    elif temp == 0 and priorityQueue.qsize() != 0:
+        print(priorityQueue.get()[1])
+    else:
+        priorityQueue.put((abs(temp), temp))
+
+sys.exit()
