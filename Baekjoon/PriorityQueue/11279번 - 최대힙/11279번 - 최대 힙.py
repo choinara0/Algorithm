25a557afb13e06bfb9db6b9dbc4cfc90fcf4e5ab
choinara0 <chlskfk6810@naver.com>
2021-10-16 15:05:48 +0900

    push 최대 힙.py solution

---
+++ Baekjoon/PriorityQueue/11279번 - 최대힙/11279번 - 최대 힙.py
@@ -0,0 +1,15 @@
+import sys
+from queue import PriorityQueue
+N = int(sys.stdin.readline())
+priorityQueue = PriorityQueue(maxsize=N)
+
+for i in range(N):
+    temp = -1 * int(sys.stdin.readline())
+    if temp == 0 and priorityQueue.qsize() == 0:
+        print(0)
+    elif temp == 0 and priorityQueue.qsize() != 0:
+        print(-1 * priorityQueue.get())
+    else:
+        priorityQueue.put(temp)
+
+sys.exit()
