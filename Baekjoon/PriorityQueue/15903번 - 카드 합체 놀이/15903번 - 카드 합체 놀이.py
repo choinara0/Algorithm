edf8141c02d318f68c942d5c8e99c89b67446ee3
choinara0 <chlskfk6810@naver.com>
2021-10-17 13:29:40 +0900

    heapq 사용해서 풀이

--- Baekjoon/PriorityQueue/15903번 - 카드 합체 놀이/15903번 - 카드 합체 놀이.py
+++ Baekjoon/PriorityQueue/15903번 - 카드 합체 놀이/15903번 - 카드 합체 놀이.py
@@ -1,3 +1,24 @@
+# heapq 사용해서 우선순위 큐 만들기
+import sys
+import heapq
+
+N, M = map(int, sys.stdin.readline().split())
+card = []
+card_list = list(map(int, sys.stdin.readline().split()))
+
+for i in card_list:
+    heapq.heappush(card, i)
+
+for j in range(M):
+    card1 = heapq.heappop(card)
+    card2 = heapq.heappop(card)
+    sumCard = card1 + card2
+
+    heapq.heappush(card, sumCard)
+    heapq.heappush(card, sumCard)
+
+print(sum(card))
+
 # priorityqueue 사용
 '''
 import sys
@@ -22,25 +43,3 @@ for i in range(priorityQueue.qsize()):
 
 print(result)
 '''
-
-# heapq 사용해서 우선순위 큐 만들기
-import sys
-import heapq
-
-N, M = map(int, sys.stdin.readline().split())
-card = []
-card_list = list(map(int, sys.stdin.readline().split()))
-
-for i in card_list:
-    heapq.heappush(card, i)
-
-for j in range(M):
-    card1 = heapq.heappop(card)
-    card2 = heapq.heappop(card)
-    sumCard = card1 + card2
-
-    heapq.heappush(card, sumCard)
-    heapq.heappush(card, sumCard)
-
-print(sum(card))
-
