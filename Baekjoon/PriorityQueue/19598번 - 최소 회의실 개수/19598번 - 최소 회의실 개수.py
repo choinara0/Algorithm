0248bdd740103fc53159663dc4f10b05ee6c4552
choinara0 <chlskfk6810@naver.com>
2021-10-18 13:01:43 +0900

    push 최소 회의실 개수.py solution

---
+++ Baekjoon/PriorityQueue/19598번 - 최소 회의실 개수/19598번 - 최소 회의실 개수.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+queue = []
+timeTable = [list(map(int, sys.stdin.readline().split())) for i in range(N)]
+timeTable.sort(key=lambda x : x[0])
+heapq.heappush(queue, timeTable[0][1])
+
+for i in range(1, N):
+    if queue[0] > timeTable[i][0]:
+        heapq.heappush(queue, timeTable[i][1])
+    else:
+        heapq.heappop(queue)
+        heapq.heappush(queue, timeTable[i][1])
+
+print(len(queue))
