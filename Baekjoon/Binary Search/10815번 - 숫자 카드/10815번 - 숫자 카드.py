d6ef9cb35edc5c0dec37a10dcfcd72622abea287
choinara0 <chlskfk6810@naver.com>
2021-10-19 16:14:53 +0900

    push 숫자 카드.py solution

---
+++ Baekjoon/Binary Search/10815번 - 숫자 카드/10815번 - 숫자 카드.py
@@ -0,0 +1,24 @@
+import sys
+
+N = int(sys.stdin.readline())
+cardList = sorted(list(map(int, sys.stdin.readline().split())))
+M = int(sys.stdin.readline())
+targetList = list(map(int, sys.stdin.readline().split()))
+start, end = 0, len(cardList) - 1
+
+def binarySearch(list, target, start, end):
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if list[mid] == target:
+            return 1
+        elif list[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+    return 0
+
+for t in targetList:
+    print(binarySearch(cardList, t, start, end), end=' ')
