c8f375ddb8564e3984e268264319b7e41ac3197d
choinara0 <chlskfk6810@naver.com>
2021-10-25 15:35:04 +0900

    push 회전 초밥.py solution

--- Baekjoon/Two Pointer/15916번 - 회전 초밥/15916번 - 회전 초밥.py
+++ Baekjoon/Two Pointer/15916번 - 회전 초밥/15916번 - 회전 초밥.py
@@ -2,23 +2,26 @@ import sys
 from collections import defaultdict
 
 N, d, k, c = map(int, sys.stdin.readline().split())
-sushiArr = [int(sys.stdin.readline().rstrip()) for i in range(N)]
-left, right, answer = 0, 0, 0
-sushiArr.extend(sushiArr)
+sushiArr = [int(sys.stdin.readline())for i in range(N)]
 intDict = defaultdict(int)
-intDict[c] += 1
-while right < k:
-    intDict[sushiArr[right]] += 1
-    right += 1
+intDict[c] = 1
+cnt = 1
 
+for i in range(k):
+    if intDict[sushiArr[i]] == 0:
+        cnt += 1
+    intDict[sushiArr[i]] += 1
 
-while right < len(sushiArr):
-    answer = max(answer, len(intDict))
-    intDict[sushiArr[left]] -= 1
-    if intDict[sushiArr[left]] == 0:
-        del intDict[sushiArr[left]]
-    intDict[sushiArr[right]] += 1
-    left += 1
-    right += 1
+answer = cnt
+for start in range(N):
+    end = (start + k) % N
+    intDict[sushiArr[start]] -= 1
+    if intDict[sushiArr[start]] == 0 :
+        cnt -= 1
+    if intDict[sushiArr[end]] == 0:
+        cnt += 1
+    intDict[sushiArr[end]] += 1
+
+    answer = max(answer, cnt)
 
 print(answer)
