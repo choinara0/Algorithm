22ac1e79e15577e614f5aeb1e64db10addde55f2
choinara0 <chlskfk6810@naver.com>
2021-11-19 17:03:19 +0900

    push 개미굴.py solution

---
+++ Baekjoon/Trie Algorithm/14725번 - 개미굴/14725번 - 개미굴.py
@@ -0,0 +1,35 @@
+import sys
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.head = Node(None)
+
+    def insert(self, string):
+
+        curr_node = self.head
+        for char in string:
+
+            if char not in curr_node.children:
+                curr_node.children[char] = Node(char)
+            curr_node = curr_node.children[char]
+
+    def print_trie(self, L, curr_node):
+        if L == 0 :
+            curr_node = self.head
+        for child in sorted(curr_node.children.keys()):
+            print("--" * L, child, sep="")
+            self.print_trie(L + 1, curr_node.children[child])
+
+
+N = int(sys.stdin.readline())
+trie = Trie()
+for i in range(N):
+    info = list(sys.stdin.readline().split())
+    trie.insert(info[1:])
+
+trie.print_trie(0, None)
