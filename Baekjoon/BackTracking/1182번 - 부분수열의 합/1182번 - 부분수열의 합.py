f146cb4aa252253d9ff46c684d6dba841a40a37e
choinara0 <chlskfk6810@naver.com>
2021-11-21 20:09:43 +0900

    backtracking으로 풀기

--- Baekjoon/BackTracking/1182번 - 부분수열의 합/1182번 - 부분수열의 합.py
+++ Baekjoon/BackTracking/1182번 - 부분수열의 합/1182번 - 부분수열의 합.py
@@ -1,4 +1,4 @@
-import sys
+'''import sys
 from itertools import combinations
 
 N, S = map(int, sys.stdin.readline().split())
@@ -10,5 +10,27 @@ for i in range(1, len(num)+1):
     for j in range(len(temp)):
         if sum(temp[j]) == S:
             answer += 1
+
+print(answer)'''
+
+# 백트래킹으로 풀기
+
+import sys
+N, S = map(int, sys.stdin.readline().split())
+num = list(map(int, sys.stdin.readline().split()))
+answer = 0
+
+def dfs(i, graph, result):
+    global answer, S
+    if result == S:
+        answer += 1
+
+    for j in range(i+1, len(num)):
+        dfs(j, num, result + num[j])
+
+    return
+
+for i in range(len(num)):
+    dfs(i, num, num[i])
 
 print(answer)
