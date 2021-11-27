6d6e6d8b2fe9014eb5797b7488ff27efe6905d8d
choinara0 <chlskfk6810@naver.com>
2021-11-27 18:23:42 +0900

    push 2xN 타일링 2.py solution

---
+++ Baekjoon/Dynamic Programming/11727번 - 2xN 타일링 2/11727번 - 2xN 타일링 2.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+dp = [0] * (n+1)
+dp[1] = 1
+
+for i in range(2, n+1):
+    if i == 2:
+        dp[2] = 3
+    else:
+        dp[i] = dp[i-2] * 2 + dp[i-1]
+
+print(dp[n] % 10007)
