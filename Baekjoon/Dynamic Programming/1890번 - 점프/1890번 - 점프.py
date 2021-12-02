d5e9980383e485263155caa6887e8496c5068067
choinara0 <chlskfk6810@naver.com>
2021-12-02 16:54:50 +0900

    push 점프.py solution

---
+++ Baekjoon/Dynamic Programming/1890번 - 점프/1890번 - 점프.py
@@ -0,0 +1,20 @@
+import sys
+
+N = int(sys.stdin.readline())
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+dp = [[0]*N for _ in range(N)]
+dp[0][0] = 1
+
+for x in range(N):
+    for y in range(N):
+        if x == N-1 and y == N-1:
+            break
+        temp = board[x][y]
+        nx, ny = x + temp, y + temp
+        if nx < N:
+            dp[nx][y] += dp[x][y]
+        if ny < N:
+            dp[x][ny] += dp[x][y]
+print(dp)
+
+
