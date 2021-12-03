50f604a01626a529f802d7f39844e6fc5b93e178
choinara0 <chlskfk6810@naver.com>
2021-12-03 23:08:12 +0900

    push 동전 1.py solution

---
+++ Baekjoon/Dynamic Programming/2293번 - 동전 1/2293번 - 동전 1.py
@@ -0,0 +1,14 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+coin = []
+for i in range(n):
+    coin.append(int(sys.stdin.readline()))
+dp = [0] * (k+1)
+dp[0] = 1
+
+for c in coin:
+    for i in range(1, k+1):
+        if i-c >= 0:
+            dp[i] += dp[i-c]
+print(dp[k])
