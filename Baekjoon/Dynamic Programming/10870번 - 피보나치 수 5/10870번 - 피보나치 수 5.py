973ba40826fa6a17409e94293cca70152befcdb5
choinara0 <chlskfk6810@naver.com>
2021-12-09 13:58:27 +0900

    push 피보나치 수 5.py solution

---
+++ Baekjoon/Dynamic Programming/10870번 - 피보나치 수 5/10870번 - 피보나치 수 5.py
@@ -0,0 +1,12 @@
+import sys
+n = int(sys.stdin.readline())
+if n == 0:
+    print(0)
+    sys.exit()
+dp = [0] * (n+1)
+dp[1] = 1
+
+for i in range(2, n+1):
+    dp[i] = dp[i-2] + dp[i-1]
+
+print(dp[n])
