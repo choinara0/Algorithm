2938fb6334661e0e7e4ccbb1162a6d6743ac1bdd
choinara0 <chlskfk6810@naver.com>
2021-12-22 22:36:25 +0900

    push 회의실 배정.py solution

--- Baekjoon/Greedy Algorithm/1931번 - 회의실 배정/1931번 - 회의실 배정.py
+++ Baekjoon/Greedy Algorithm/1931번 - 회의실 배정/1931번 - 회의실 배정.py
@@ -1,3 +1,12 @@
 import sys
 N = int(sys.stdin.readline())
 meeting = [list(map(int, sys.stdin.readline().split())) for i in range(N)]
+meeting.sort(key = lambda x:(x[1], x[0]))
+cnt = 1
+endTime = meeting[0][1]
+for i in range(1, N):
+    if meeting[i][0] >= endTime:
+        cnt += 1
+        endTime = meeting[i][1]
+
+print(cnt)
