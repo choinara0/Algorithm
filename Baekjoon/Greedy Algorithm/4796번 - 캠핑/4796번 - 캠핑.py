0d20ceeacd2b38413749eb52887f37149c2376b3
choinara0 <chlskfk6810@naver.com>
2021-12-25 23:11:28 +0900

    push 캠핑.py solution

---
+++ Baekjoon/Greedy Algorithm/4796번 - 캠핑/4796번 - 캠핑.py
@@ -0,0 +1,20 @@
+import sys
+caseCnt = 0
+while True:
+    caseCnt += 1
+    L, P, V = map(int, sys.stdin.readline().split())
+    if L == 0 and P == 0 and V == 0:
+        break
+    day = 0
+    while True:
+        if V >= P:
+            V -= P
+            day += L
+        elif P >= V >= L:
+            V -= V
+            day += L
+        else:
+            day += V
+            break
+
+    print('Case ' + str(caseCnt) + ': ' + str(day))
