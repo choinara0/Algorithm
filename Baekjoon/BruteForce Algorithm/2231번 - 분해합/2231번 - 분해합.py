9f4ad8091c95148d21b1afd424c10a28d005663e
choinara0 <chlskfk6810@naver.com>
2022-01-12 12:49:04 +0900

    push 분해합.py solution

---
+++ Baekjoon/BruteForce Algorithm/2231번 - 분해합/2231번 - 분해합.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for i in range(1, n+1):
+    num = sum(map(int, str(i)))
+    num_sum = num + i
+
+    if num_sum == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
