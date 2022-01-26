622168dc51bbd3f478de56e17ca1760b360df02f
choinara0 <chlskfk6810@naver.com>
2022-01-26 12:47:38 +0900

    push 한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py solution

---
+++ Baekjoon/BruteForce Algorithm/2422번 - 한윤정이 이탈리아에 가서 아이스크림을 사먹는데/2422번 - 한윤정이 이탈리아에 가서 아이스크림을 사먹는데.py
@@ -0,0 +1,19 @@
+import sys
+from itertools import combinations
+
+N, M = map(int, sys.stdin.readline().split())
+ice_combination = list(combinations(range(1, N+1), 3))
+no_combination = [[0] * (N+1) for i in range(N+1)]
+answer = 0
+
+for i in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    no_combination[a][b] = 1
+    no_combination[a][b] = 1
+
+for ice_comb in ice_combination:
+    if no_combination[ice_comb[0]][ice_comb[1]] or no_combination[ice_comb[0]][ice_comb[2]] or no_combination[ice_comb[1]][ice_comb[2]]:
+        continue
+    answer += 1
+
+print(answer)
