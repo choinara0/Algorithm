bbe27ed8f348eb57d2200305b2ae85274d30b27c
choinara0 <chlskfk6810@naver.com>
2022-01-27 16:56:33 +0900

    replace files

---
+++ Baekjoon/Dynamic Programming/1535번 - 안녕/1535번 - 안녕.py
@@ -0,0 +1,12 @@
+import sys
+from itertools import combinations
+
+N = int(sys.stdin.readline())
+life_point = list(map(int, sys.stdin.readline().split()))
+happy_point = list(map(int, sys.stdin.readline().split()))
+
+
+answer = float('-Inf')
+
+for i in range(1, N+1):
+    for comb in combinations()
