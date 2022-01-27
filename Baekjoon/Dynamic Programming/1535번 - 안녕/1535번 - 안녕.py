c992ba727fbc47bb177d469afe1d69d059f374ae
choinara0 <chlskfk6810@naver.com>
2022-01-27 17:02:48 +0900

    push 안녕.py solution

--- Baekjoon/Dynamic Programming/1535번 - 안녕/1535번 - 안녕.py
+++ Baekjoon/Dynamic Programming/1535번 - 안녕/1535번 - 안녕.py
@@ -1,12 +1,15 @@
 import sys
-from itertools import combinations
 
 N = int(sys.stdin.readline())
-life_point = list(map(int, sys.stdin.readline().split()))
-happy_point = list(map(int, sys.stdin.readline().split()))
-
-
-answer = float('-Inf')
+life_point = [0] + list(map(int, sys.stdin.readline().split()))
+happy_point = [0] + list(map(int, sys.stdin.readline().split()))
+dp = [[0]*101 for i in range(N+1)]
 
 for i in range(1, N+1):
-    for comb in combinations()
+    for j in range(1, 101):
+        if life_point[i] <= j :
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - life_point[i]] + happy_point[i])
+        else:
+            dp[i][j] = dp[i - 1][j]
+
+print(dp[N][99])
