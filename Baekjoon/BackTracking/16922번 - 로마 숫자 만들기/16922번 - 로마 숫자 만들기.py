f414cd889262f0155bbfb1b46b6fd80031aef0c8
choinara0 <chlskfk6810@naver.com>
2022-02-03 14:25:27 +0900

    push 로마 숫자 만들기.py solution

---
+++ Baekjoon/BackTracking/16922번 - 로마 숫자 만들기/16922번 - 로마 숫자 만들기.py
@@ -0,0 +1,35 @@
+# combinations_with_replacement로 풀기
+import sys
+from itertools import combinations_with_replacement
+
+N = int(sys.stdin.readline())
+arr = [1, 5, 10, 50]
+answer = set()
+for comb in combinations_with_replacement(arr, N):
+    answer.add(sum(comb))
+
+print(len(answer))
+
+# backtracking으로 풀기
+N = int(sys.stdin.readline())
+number = [1, 5, 10, 50]
+numberList = []
+sumList = [0] * 1001
+
+
+def backtrack(depth, num):
+    try:
+        if depth == N:
+            sumList[sum(numberList)] = 1
+            return
+
+        for i in range(num, 4):
+            numberList.append(number[i])
+            backtrack(depth + 1, i)
+            numberList.pop()
+    except:
+        print(len(sumList))
+
+
+backtrack(0, 0)
+print(sum(sumList))
