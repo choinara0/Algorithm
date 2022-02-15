e3d52a1c54007d1089fbd2bd2765927248e23e39
choinara0 <chlskfk6810@naver.com>
2022-02-15 11:33:56 +0900

    push Small World.py solution

--- Baekjoon/Graph Algorithm/24479번 - 알고리즘 수업 - 깊이 우선 탐색 1 /24479번 - 알고리즘 수업 - 깊이 우선 탐색 1.py
+++ Baekjoon/Graph Algorithm/24479번 - 알고리즘 수업 - 깊이 우선 탐색 1 /24479번 - 알고리즘 수업 - 깊이 우선 탐색 1.py
@@ -1,20 +1,24 @@
 import sys
 sys.setrecursionlimit(10**5)
+
 N, M, R = map(int, sys.stdin.readline().split())
 graph = [[]*(N+1) for _ in range(N+1)]
 visited = [0] * (N+1)
+
 for _ in range(M):
     a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def dfs(bx, x):
-    visited[x] = visited[bx] + 1
+def dfs(x, c):
+    visited[x] = c
     for nx in sorted(graph[x]):
         if not visited[nx]:
-            dfs(x, nx)
-    return
+            c += 1
+            visited[nx] = c
+            dfs(nx, c)
 
-dfs(R, R)
-for i in range(1, N+1):
-    print(visited[i])
+dfs(R, 1)
+
+for _ in range(1, N+1):
+    print(visited[_])
