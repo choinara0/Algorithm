3fe1eaa84649a66ac20f622b067f9961f8613dd9
choinara0 <chlskfk6810@naver.com>
2022-02-15 16:19:56 +0900

    push 알고리즘 수업 - 깊이 우선 탐색 1.py solution

--- Baekjoon/Graph Algorithm/24479번 - 알고리즘 수업 - 깊이 우선 탐색 1 /24479번 - 알고리즘 수업 - 깊이 우선 탐색 1.py
+++ Baekjoon/Graph Algorithm/24479번 - 알고리즘 수업 - 깊이 우선 탐색 1 /24479번 - 알고리즘 수업 - 깊이 우선 탐색 1.py
@@ -1,24 +1,24 @@
 import sys
-sys.setrecursionlimit(10**5)
+sys.setrecursionlimit(10**6)
 
 N, M, R = map(int, sys.stdin.readline().split())
 graph = [[]*(N+1) for _ in range(N+1)]
 visited = [0] * (N+1)
+cnt = 1
 
 for _ in range(M):
     a, b = map(int, sys.stdin.readline().split())
     graph[a].append(b)
     graph[b].append(a)
 
-def dfs(x, c):
-    visited[x] = c
+def dfs(x):
+    global cnt
+    visited[x] = cnt
+    cnt += 1
     for nx in sorted(graph[x]):
         if not visited[nx]:
-            c += 1
-            visited[nx] = c
-            dfs(nx, c)
+            dfs(nx)
 
-dfs(R, 1)
+dfs(R)
 
-for _ in range(1, N+1):
-    print(visited[_])
+print(*[visited[x] for x in range(1, N+1)], sep='\n')
