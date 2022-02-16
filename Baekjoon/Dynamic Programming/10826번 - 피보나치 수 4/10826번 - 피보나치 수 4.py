bce47c9cca8dfc2477179f0e1e2c5aafd033ff35
choinara0 <chlskfk6810@naver.com>
2022-02-16 14:52:59 +0900

    index error 수정

--- Baekjoon/Dynamic Programming/10826번 - 피보나치 수 4/10826번 - 피보나치 수 4.py
+++ Baekjoon/Dynamic Programming/10826번 - 피보나치 수 4/10826번 - 피보나치 수 4.py
@@ -3,13 +3,13 @@ import sys
 n = int(sys.stdin.readline())
 dp = [0] * (n+1)
 dp[1] = 1
+dp[2] = 1
 
 if n == 0:
     print(0)
-elif n == 1:
+elif n == 1 and n == 2:
     print(1)
 else:
-    for i in range(2, n+1):
+    for i in range(3, n+1):
         dp[i] = dp[i-2] + dp[i-1]
-
     print(dp[n])
