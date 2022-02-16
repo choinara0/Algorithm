a7b625df8d5fc8f9c09b49debb46a5d0c298cc04
choinara0 <chlskfk6810@naver.com>
2022-02-16 17:54:13 +0900

    push 알고리즘 수업 - 너비 우선 탐색 1 README. md

---
+++ Baekjoon/Graph Algorithm/24444번 - 알고리즘 수업 - 너비 우선 탐색 1/24444번 - 알고리즘 수업 - 너비 우선 탐색 1.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+N, M, R = map(int, sys.stdin.readline().split())
+graph = [[] * (N+1) for _ in range(N+1)]
+visited = [0] * (N+1)
+
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+def bfs(x):
+    q = deque()
+    q.append(x)
+    visited[x] = 1
+    cnt = 1
+
+    while q:
+        x = q.popleft()
+        for nx in sorted(graph[x]):
+            if not visited[nx]:
+                cnt += 1
+                q.append(nx)
+                visited[nx] = cnt
+    return
+
+bfs(R)
+
+for _ in range(1, N+1):
+    print(visited[_])
