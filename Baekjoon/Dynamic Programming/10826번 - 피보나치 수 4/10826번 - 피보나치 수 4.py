05796773b33a0411b88e1c4143588425fc90b24b
choinara0 <chlskfk6810@naver.com>
2022-02-16 14:44:06 +0900

    push 피보나치 수.py solution

---
+++ Baekjoon/Dynamic Programming/10826번 - 피보나치 수 4/10826번 - 피보나치 수 4.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+dp = [0] * (n+1)
+dp[1] = 1
+
+if n == 0:
+    print(0)
+elif n == 1:
+    print(1)
+else:
+    for i in range(2, n+1):
+        dp[i] = dp[i-2] + dp[i-1]
+
+    print(dp[n])
