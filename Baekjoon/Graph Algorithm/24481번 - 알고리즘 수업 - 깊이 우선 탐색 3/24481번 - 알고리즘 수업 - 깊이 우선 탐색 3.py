337aa9dad12a6c4b853dd720266f8f1247984af7
choinara0 <chlskfk6810@naver.com>
2022-02-16 16:58:51 +0900

    push 알고리즘 수업 - 깊이 우선 탐색 3.py solution

---
+++ Baekjoon/Graph Algorithm/24481번 - 알고리즘 수업 - 깊이 우선 탐색 3/24481번 - 알고리즘 수업 - 깊이 우선 탐색 3.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10**6)
+N, M, R = map(int, sys.stdin.readline().split())
+graph = [[]*(N+1) for _ in range(N+1)]
+visited = [0] * (N+1)
+depth = [-1] * (N+1)
+
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(bx, x):
+    visited[x] = 1
+    depth[x] = depth[bx] + 1
+
+    for nx in sorted(graph[x]):
+        if not visited[nx]:
+            dfs(x, nx)
+
+dfs(R, R)
+
+for i in range(1, N+1):
+    print(depth[i])
