b15bee547e9b9b362132d914db3b1ba44e62a681
choinara0 <chlskfk6810@naver.com>
2022-02-21 00:02:35 +0900

    push 거스름돈.py solution

---
+++ Programmer/Level 3/거스름돈/거스름돈.py
@@ -0,0 +1,11 @@
+def solution(n, money):
+    dp = [1] + [0] * n
+
+    for coin in money:
+        for price in range(coin, n+1):
+            if price >= coin:
+                dp[price] += dp[price-coin]
+
+    return dp[n] % 1000000007
+
+solution(5, [1, 2, 5]) #4
