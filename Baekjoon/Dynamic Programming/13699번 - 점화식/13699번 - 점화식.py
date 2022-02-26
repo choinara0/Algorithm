4f8058901bf1cfe7a8147df73cd72bcde204336f
choinara0 <chlskfk6810@naver.com>
2022-02-26 22:26:33 +0900

    push 점화식.py solution

---
+++ Baekjoon/Dynamic Programming/13699번 - 점화식/13699번 - 점화식.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline())
+dp = [0] * (n+1)
+dp[0] = 1
+
+for i in range(1, n+1):
+    for j in range(0, i, 1):
+        dp[i] += dp[j] * dp[i-j-1]
+
+print(dp[n])
