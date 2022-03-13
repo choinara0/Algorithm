62b53f86366e828b7e0270fc5e2ab9f40799ae35
choinara0 <chlskfk6810@naver.com>
2022-03-13 12:59:07 +0900

    push AC.py solution

---
+++ Baekjoon/String/5430번 - AC/5430번 - AC.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    p = sys.stdin.readline().strip()
+    n = int(sys.stdin.readline())
+    arr = deque(sys.stdin.readline().strip()[1:-1].split(","))
+    check = False
+    R_cnt = 0
+    if arr[0] == '':
+        arr= deque()
+
+    for i in p:
+        if i == 'R':
+            R_cnt += 1
+        if i == 'D':
+            if not arr:
+                print("error")
+                check = True
+                break
+            if R_cnt % 2 == 0:
+                arr.popleft()
+            else:
+                arr.pop()
+    if R_cnt % 2 != 0:
+        arr.reverse()
+
+    if not check:
+        print('[' + ",".join(arr) + "]")
