dcc0eaaf152282acbe5736eec741e6def4379c98
choinara0 <chlskfk6810@naver.com>
2022-03-29 20:44:53 +0900

    push 평범한 배낭.py solution

---
+++ Baekjoon/Dynamic Programming/12865번 - 평범한 배낭/12865번 - 평범한 배낭.py
@@ -0,0 +1,17 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+arr = [[0,0]] + [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+dp = [[0] * (K+1) for _ in range(N+1)]
+
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        w = arr[i][0]
+        v = arr[i][1]
+
+        if j < w:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w] + v)
+
+print(dp[N][K])
