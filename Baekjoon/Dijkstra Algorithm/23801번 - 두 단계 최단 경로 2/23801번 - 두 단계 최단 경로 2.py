9a5eccf5b81ea3b1cebffe669a9736f2ca5822cc
choinara0 <chlskfk6810@naver.com>
2022-04-14 00:53:44 +0900

    push 두 단계 최단 경로 2.py solution

--- Baekjoon/Dijkstra Algorithm/23801번 - 두 단계 최단 경로 2/23801번 - 두 단계 최단 경로 2.py
+++ Baekjoon/Dijkstra Algorithm/23801번 - 두 단계 최단 경로 2/23801번 - 두 단계 최단 경로 2.py
@@ -12,12 +12,11 @@ def dijkstra(start):
         if dist[current_node] < current_cost:
             continue
 
-        for next_cost, next_node in graph[current_cost]:
+        for next_cost, next_node in graph[current_node]:
             sum_cost = next_cost + current_cost
             if dist[next_node] > sum_cost:
                 dist[next_node] = sum_cost
                 heapq.heappush(heap, [sum_cost, next_node])
-
     return dist
 
 input = sys.stdin.readline
@@ -30,10 +29,9 @@ for _ in range(M):
 X, Z = map(int, input().split())
 P = int(input())
 Y = list(map(int, input().split()))
-
+answer = sys.maxsize
 x_arr = dijkstra(X)
-print(x_arr)
-p_arr = []
-for i in Y:
-    p_arr.append(dijkstra(i))
-print(p_arr)
+z_arr = dijkstra(Z)
+for p in Y:
+    answer = min(answer, x_arr[p] + z_arr[p])
+print(answer if answer != sys.maxsize else -1)
