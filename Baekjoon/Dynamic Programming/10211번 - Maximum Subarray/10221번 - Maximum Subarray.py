1492298513dde2ae286bd221b6d3f9c52412d960
choinara0 <chlskfk6810@naver.com>
2022-04-24 11:25:16 +0900

    push Maximum Subarray.py solution

--- Baekjoon/Dynamic Programming/10211번 - Maximum Subarray/10221번 - Maximum Subarray.py
+++ Baekjoon/Dynamic Programming/10211번 - Maximum Subarray/10221번 - Maximum Subarray.py
@@ -7,10 +7,8 @@ for _ in range(T):
     arr = list(map(int, input().split()))
     dp = [0] * N
     dp[0] = arr[0]
-    max_dp = -10000000
+
     for i in range(1, N):
         dp[i] = max(dp[i-1] + arr[i], arr[i])
-        if dp[i] > max_dp:
-            max_dp = dp[i]
 
-    print(max_dp)
+    print(max(dp))
