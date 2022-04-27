3e0a97a2455b9764763ba1cdc50c6b137b8ea4e3
choinara0 <chlskfk6810@naver.com>
2022-04-27 16:24:53 +0900

    push 숫자 재배치.py solution

--- Baekjoon/BackTracking/16943번 - 숫자 재배치/16943번 - 숫자 재배치.py
+++ Baekjoon/BackTracking/16943번 - 숫자 재배치/16943번 - 숫자 재배치.py
@@ -1,4 +1,26 @@
 import sys
 
-N, M, K = map(int, sys.stdin.readline().split())
-board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+def dfs(cnt, sum_val):
+    global answer
+
+    if cnt == len_a:
+        if int(sum_val) <= b:
+            answer = max(answer, int(sum_val))
+            return
+    for i in range(len_a):
+        if cnt == 0 and a[i] == 0:
+            continue
+        if not check[i]:
+            check[i] = True
+            dfs(cnt+1, sum_val + str(a[i]))
+            check[i] = False
+
+    return
+
+a, b = map(int, sys.stdin.readline().split())
+a = list(map(int, str(a)))
+len_a = len(a)
+check = [False] * len_a
+answer = -1
+dfs(0, '')
+print(answer)
