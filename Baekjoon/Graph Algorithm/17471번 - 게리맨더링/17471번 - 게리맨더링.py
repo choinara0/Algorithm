518aabc7dbafa2aa0b7efcc53635ac382d416192
choinara0 <chlskfk6810@naver.com>
2022-05-03 19:35:06 +0900

    Add files

---
+++ Baekjoon/Graph Algorithm/17471번 - 게리맨더링/17471번 - 게리맨더링.py
@@ -0,0 +1,42 @@
+import sys
+from collections import deque
+from itertools import combinations
+
+def bfs(arr):
+    start = arr[0]
+    q = deque()
+    q.append(start)
+    visited = set([start])
+    num = 0
+
+    while q:
+        value = q.popleft()
+        num += people[value]
+        for i in graph[value]:
+            if i not in visited and i in arr:
+                q.append(i)
+                visited.add(i)
+
+    return num, len(visited)
+
+input = sys.stdin.readline
+
+N = int(input())
+people = [0] + list(map(int, input().split()))
+graph = [[] for _ in range(N+1)]
+result = sys.maxsize
+
+for i in range(1, N+1):
+    graph[i] = list(map(int, input().split()))
+    graph[i] = graph[i][1:]
+
+for i in range(1, N//2+1):
+    combies = list(combinations(range(1, N+1), i))
+    for comb in combies:
+        sum1, temp1 = bfs(comb)
+        sum2, temp2 = bfs([i for i in range(1, N+1) if i not in comb])
+
+    if temp1 + temp2 == N:
+        result = min(result, abs(sum1 -sum2))
+
+print(result)
