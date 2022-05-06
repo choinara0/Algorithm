e3fc95de1d1a3a1283618cead2bd07983dca6fe1
choinara0 <chlskfk6810@naver.com>
2022-05-06 22:13:04 +0900

    Add files

---
+++ Baekjoon/Binary Search/13702번 - 이상한 술집/13702번 - 이상한 술집.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+arr = [int(input()) for _ in range(N)]
+left, right = 1, arr[0]
+answer = 0
+
+while left <= right:
+    mid = (left + right) // 2
+    people = 0
+    for n in arr:
+        people += (n // mid)
+
+    if people < K:
+        right = mid - 1
+    else:
+        left = mid + 1
+        answer = mid
+
+print(answer)
