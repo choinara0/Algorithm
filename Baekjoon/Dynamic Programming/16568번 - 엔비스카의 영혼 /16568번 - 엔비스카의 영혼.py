7d8ae710448edf3b99e34c8689ebbe8f8ae8e18c
choinara0 <chlskfk6810@naver.com>
2023-09-21 22:34:44 +0900

    move files

---
+++ Baekjoon/Dynamic Programming/16568번 - 엔비스카의 영혼 /16568번 - 엔비스카의 영혼.py
@@ -0,0 +1,15 @@
+import sys
+
+N, a, b = map(int, sys.stdin.readline().split())
+dp = [float('inf')] * 1000001
+dp[N] = 0
+
+for i in range(N, -1, -1):
+    if i-1 >= 0:
+        dp[i-1] = min(dp[i-1], dp[i] + 1)
+    if i-a-1 >= 0:
+        dp[i-a-1] = min(dp[i-a-1], dp[i] + 1)
+    if i-b-1 >= 0:
+        dp[i-b-1] = min(dp[i-b-1], dp[i] + 1)
+
+print(dp[0])
